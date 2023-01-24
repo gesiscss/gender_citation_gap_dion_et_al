8dd3a3204381dc8af0ae23bdf4edeb5e822ebc96
mio-hiehei <mio.hiehei@gmail.com>
2023-01-24 13:39:44 +0100

    use posit pm as source

---
+++ .binder/install.R
@@ -0,0 +1,6 @@
+options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/cran/2023-01-23"))
+
+install.packages(c("tidyverse", "foreign", "irdisplay", "irkernel", "optimx", "rms", "kableExtra"))
+
+
+
